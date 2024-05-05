e0276bd9063caf86b5c9e16a5214c621e9295329
Felipe Spider <feliprerobinsonprofissional@gmail.com>
2024-05-05 20:05:08 -0300

    Update

--- PONG JS/sketch.js
+++ PONG JS/sketch.js
@@ -1,41 +1,64 @@
-//variáveis da bolinha
+// nossas VARIÁVEIS da BOLINHA
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 15;
-let raio = diametro / 2;
+let dBolinha = 10;
+let rBolinha = dBolinha / 2;
 
-//velocidade da bolinha
-let velocidadeXBolinha = 2;
-let velocidadeYBolinha = 2;
+let velBolinhaX = 8;
+let velBolinhaY = 8;
 
+// nossas VARIÁVEIS da RAQUETE AMIGA
+let xRaquet = 10
+let yRaquet = 150
+let largRaquete = 10
+let altRaquete = 90
+
+// nossas FUNCTIONS
 function setup() {
     createCanvas(600,400);
+    // esse é o tamanho da tela onde está rodando o jogo
 }
 
 function draw() {
     background(0);
     mostraBolinha();
-    movimentaBolinha();
-    verificaColisaoBorda();
+    movimentacaoBolinha();
+    verificacaoDaBorda();
+    raquetola();
+    moviementoRaquetola();
+    // aqui fizemos as refatorações do código, para aplicar melhor
 }
 
 function mostraBolinha() {
-    circle(xBolinha, yBolinha, diametro)
+    circle(xBolinha, yBolinha, dBolinha)
+    // nesse circle, temos as definições desse objeto:
+    // (x,y = eixo aonde a bolinha ira se encontrar)
+    // (d = diametro da bolinha, o seu tamanho)
 }
 
-function movimentaBolinha() {
-    xBolinha += velocidadeXBolinha;
-    yBolinha += velocidadeYBolinha;
+function movimentacaoBolinha() {
+    xBolinha += velBolinhaX;
+    yBolinha += velBolinhaY;
 }
 
-function verificaColisaoBorda() {
-    if (xBolinha + raio > width || xBolinha - raio < 0) {
-        velocidadeXBolinha *= -1;
+function verificacaoDaBorda() {
+    if (xBolinha + rBolinha > width || xBolinha - rBolinha < 0) {
+        velBolinhaX *= -1;
     }
-    if (yBolinha + raio > height || yBolinha - raio < 0) {
-        velocidadeYBolinha *= -1;
+    if (yBolinha + rBolinha > height || yBolinha - rBolinha < 0) {
+        velBolinhaY *= -1;
     }
 }
 
-// lá em cima, o let dá os valores a variável, o x e o y é aonde a bolinha vai
-// se localizar na tela, e o d é o diametro da bolinha 
+function raquetola() {
+    rect(xRaquet, yRaquet, largRaquete, altRaquete)
+}
+
+function moviementoRaquetola() {
+    if(keyIsDown(UP_ARROW)) {
+        yRaquet -= 10;
+    }
+    if(keyIsDown(DOWN_ARROW)) {
+        yRaquet += 10;
+    }
+}
